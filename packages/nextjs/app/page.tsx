9b1efacbcfb5df8aace59c6c100926ab4b8bee19
“Oscar <edsphinx@gmail.com>
2025-09-21 23:50:15 -0600

    fix: Dashboard to english

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -13,9 +13,9 @@ const Home: NextPage = () => {
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-4xl font-bold">Protocolo Sunny ☀️</span>
+            <span className="block text-4xl font-bold">Veritas Protocol</span>
           </h1>
-          <p className="text-center text-lg">Dashboard de Pruebas Interactivo</p>
+          <p className="text-center text-lg">Dashboard</p>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
